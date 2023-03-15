46f19c0a8d296965fe9670f9087f88a14ded60ab
BraveB <bravogbeat@outlook.es>
2023-03-15 09:42:54 -0600

    ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'dbz-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
-  agregar(){
+
+  nuevo:Personaje={
+    nombre: 'Trunks',
+    poder: 1400
   }
+
+  agregar(){
+    console.log(this.nuevo)
+  }
+  // cambiarNombre(event:any){
+  //   console.log(event.target.value);
+  // }
 }
