63845fcefb8c56e21465adbc308a54650cf5c8d3
BraveB <bravogbeat@outlook.es>
2023-03-22 14:35:04 -0600

    Centralizar  el acceso de los personajes en el servicio

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -7,24 +7,8 @@ import { DbzService } from '../services/dbz.service';
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
-
-  nuevo: Personaje = {
+    nuevo: Personaje = {
     nombre: '',
     poder: 0
   }
-  personajes: Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder:1000
-    },
-    {
-      nombre: 'Vegeta',
-      poder:2000
-    },
-  ];
-  agregarNuevoPersonaje(nuevoPersonaje:Personaje){
-    this.personajes.push(nuevoPersonaje)
-  }
-
-  constructor( private dbzService:DbzService ) {}
 }
